cfa23507741bb9207347da0bba4e9ef307bb08da
angelicalombas <angelica_lombas@hotmail.com>
2022-04-06 11:51:24 -0300

    exercicio 2

---
+++ 0x01/lsp/BankCard.java
@@ -0,0 +1,12 @@
+public class BankCard implements IPayment{
+
+    @Override
+    public void validate() throws Exception {
+
+    }
+
+    @Override
+    public void collectPayment() {
+        System.out.println("Pagamento Realizado");
+    }
+}
---
+++ 0x01/lsp/CreditCard.java
@@ -0,0 +1,8 @@
+public class CreditCard extends BankCard{
+
+    @Override
+    public void validate() throws Exception {
+        System.out.println("Verificando Limite!");
+        System.out.println("Limite disponível!");
+    }
+}
